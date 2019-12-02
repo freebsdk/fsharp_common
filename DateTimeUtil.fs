0fbada355422a3b13351014451cc9c11dbd18263
SeokYoung Shin <aceorb@gmail.com>
2019-12-03 01:26:49 +0900

    .

---
+++ DateTimeUtil.fs
@@ -0,0 +1,25 @@
+﻿namespace fsharp_common
+open System
+
+
+
+
+
+
+module DateTimeUtil =
+    
+    let CurUnixTimeMSec() =
+        DateTimeOffset.Now.ToUnixTimeMilliseconds()
+        
+        
+        
+        
+        
+    let UnixTimeMSec2DateTime(unixTimeMSec : int64) =
+        DateTimeOffset.FromUnixTimeMilliseconds(unixTimeMSec).DateTime
+                
+    
+    
+    
+    let DateTime2UnixTimeMSec(dateTime : DateTime) =
+        DateTimeOffset(dateTime).ToUnixTimeMilliseconds()
---
+++ Library.fs
@@ -0,0 +1,5 @@
+﻿namespace fsharp_common
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
